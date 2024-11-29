e6eeea85cf1f0f3508dafa50520d8a76e8f5fc43
Roman <s-3m@yandex.ru>
2024-11-29 21:17:56 +0300

    rework with compare with files

--- msk/main.py
+++ msk/main.py
@@ -11,136 +11,198 @@ from loguru import logger
 import pandas.io.formats.excel
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from utils import check_danger_string
+from utils import (
+    check_danger_string,
+    filesdata_to_dict,
+    fetch_request,
+    write_result_files,
+)
 
 pandas.io.formats.excel.ExcelFormatter.header_style = None
 
+DEBUG = False
 BASE_URL = "https://www.moscowbooks.ru/"
 USER_AGENT = UserAgent()
+BASE_LINUX_DIR = "/media/source/msk" if not DEBUG else "source"
+semaphore = asyncio.Semaphore(10)
+logger.add(
+    f"{BASE_LINUX_DIR}/error.log",
+    format="{time} {level} {message}",
+    level="ERROR",
+)
+logger.add(
+    f"{BASE_LINUX_DIR}/error_serialize.json",
+    format="{time} {level} {message}",
+    level="ERROR",
+    serialize=True,
+)
 headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "user-agent": USER_AGENT.random,
 }
+
 count = 1
 result = []
+item_error = []
+page_error = []
+id_to_add = []
+id_to_del = []
+
+prices = filesdata_to_dict(f"{BASE_LINUX_DIR}/prices")
+df_price_one = prices["1"]
+df_price_two = prices["2"]
+df_price_three = prices["3"]
+sample = filesdata_to_dict(f"{BASE_LINUX_DIR}/sale", combined=True)
+not_in_sale = filesdata_to_dict(f"{BASE_LINUX_DIR}/not_in_sale", combined=True)
 
 
 async def get_item_data(session, item):
     global count
     link = BASE_URL + item
     try:
-        async with session.get(link, headers=headers) as resp:
-            soup = bs(await resp.text(), "lxml")
-            age_control = soup.find("input", id="age_verification_form_mode")
-            script_index = 1
-            if age_control:
-                logger.warning("Age Control found")
-                closed_page = get_book_data(link)
-                soup = bs(closed_page, "lxml")
-                script_index = 5
+        # async with session.get(link, headers=headers) as resp:
+        resp = await fetch_request(session, link, headers)
+        soup = bs(await resp.text(), "lxml")
+        age_control = soup.find("input", id="age_verification_form_mode")
+        script_index = 1
+        if age_control:
+            logger.warning("Age Control found")
+            closed_page = get_book_data(link)
+            soup = bs(closed_page, "lxml")
+            script_index = 5
 
-            # Article
-            try:
-                article = link.split("/")[-1]
-            except:
-                article = "Нет артикула"
-            # Book title
-            try:
-                title = soup.find("h1").text.strip()
-                title = await check_danger_string(title, "title")
-                if not title:
-                    return
-            except:
-                title = "Название не указано"
+        # Article
+        try:
+            article = link.split("/")[-1]
+        except:
+            article = "Нет артикула"
+        # Book title
+        try:
+            title = soup.find("h1").text.strip()
+            title = await check_danger_string(title, "title")
+            if not title:
+                return
+        except:
+            title = "Название не указано"
 
-            # Book author
-            try:
-                author_div = soup.find("div", class_="page-header__author")
-                author_list = [i.text.strip() for i in author_div.find_all("a")]
-                author = ", ".join(author_list)
-            except:
-                author = "Автор не указан"
+        # Book author
+        try:
+            author_div = soup.find("div", class_="page-header__author")
+            author_list = [i.text.strip() for i in author_div.find_all("a")]
+            author = ", ".join(author_list)
+        except:
+            author = "Автор не указан"
 
-            # Photo
-            try:
-                img = soup.find("div", class_="book__cover").find("a").get("href")
-            except:
-                img = "Нет фото"
+        # Photo
+        try:
+            img = soup.find("div", class_="book__cover").find("a").get("href")
+        except:
+            img = "Нет фото"
 
-            # Description
-            try:
-                description_list = [
-                    i.next_element.text.strip()
-                    for i in soup.find("div", class_="book__description").find_all("br")
-                    if i.next_element.text != ""
-                ]
-                description = "\n".join(description_list)
-                description = await check_danger_string(description, "description")
-            except:
-                description = "Нет описания"
+        # Description
+        try:
+            description_list = [
+                i.next_element.text.strip()
+                for i in soup.find("div", class_="book__description").find_all("br")
+                if i.next_element.text != ""
+            ]
+            description = "\n".join(description_list)
+            description = await check_danger_string(description, "description")
+        except:
+            description = "Нет описания"
 
-            # Category
-            try:
-                category = soup.find_all("li", class_="breadcrumbs__item")[
-                    -2
-                ].text.strip()
-            except:
-                category = "Без категории"
+        # Category
+        try:
+            category = soup.find_all("li", class_="breadcrumbs__item")[-2].text.strip()
+        except:
+            category = "Без категории"
 
-            # Price
-            try:
-                price = soup.find("div", class_="book__price").text.strip()
-            except:
-                price = "Цена не указана"
+        # Price
+        try:
+            price = soup.find("div", class_="book__price").text.strip()
+        except:
+            price = "Цена не указана"
 
-            # Stock
-            try:
-                need_element = soup.find_all("script")
-                a = (
-                    need_element[script_index]
-                    .text.split("MbPageInfo = ")[1]
-                    .replace("false", "False")
-                    .replace("true", "True")
-                )
-                need_data_dict = eval(a[:-1])["Products"][0]
-                stock = need_data_dict["Stock"]
-            except:
-                stock = "Количество не указано"
+        # Stock
+        try:
+            need_element = soup.find_all("script")
+            a = (
+                need_element[script_index]
+                .text.split("MbPageInfo = ")[1]
+                .replace("false", "False")
+                .replace("true", "True")
+            )
+            need_data_dict = eval(a[:-1])["Products"][0]
+            stock = need_data_dict["Stock"]
+        except:
+            stock = "Количество не указано"
 
-            # Details
-            try:
-                all_details = soup.find_all("dl", class_="book__details-item")
-                details_dict = {
-                    i.find_all()[0].text.strip(): i.find_all()[1].text.strip()
-                    for i in all_details
-                }
-            except:
-                details_dict = {}
-
-            book_dict = {
-                "link": link,
-                "article": article,
-                "title": title,
-                "author": author,
-                "description": description,
-                "category": category,
-                "price": price,
-                "stock": stock,
-                "image": img,
+        # Details
+        try:
+            all_details = soup.find_all("dl", class_="book__details-item")
+            details_dict = {
+                i.find_all()[0].text.strip().split(":")[0]: i.find_all()[1].text.strip()
+                for i in all_details
             }
-            book_dict.update(details_dict)
+        except:
+            details_dict = {}
 
-            result.append(book_dict)
-            print(f"\rDone - {count}", end="")
-            count += 1
+        book_dict = {
+            "link": link,
+            "article": article,
+            "title": title,
+            "author": author,
+            "description": description,
+            "category": category,
+            "price": price,
+            "stock": stock,
+            "image": img,
+        }
+        book_dict.update(details_dict)
+
+        article_for_check = article + ".0"
+        item_status = soup.find("div", class_="book__buy")
+        for d in [df_price_one, df_price_two, df_price_three]:
+            if article_for_check in d and item_status is not None:
+                d[article]["price"] = price
+
+        if article_for_check in not_in_sale and item_status:
+            not_in_sale[article]["on sale"] = "да"
+        if article not in sample and item_status:
+            id_to_add.append(book_dict)
+        if article in sample and item_status is None:
+            id_to_del.append({"article": article})
+
+        result.append(book_dict)
+        print(f"\rDone - {count}", end="")
+        count += 1
     except Exception as e:
+        item_error.append(link)
         logger.exception(f"Exception in book - {link}")
         with open("error.txt", "a+") as file:
             file.write(f"{link} ---> {e}\n")
 
 
+async def get_page_data(session, page_link, tasks):
+    async with semaphore:
+        try:
+            page_response = await fetch_request(session, page_link, headers)
+            page_html = await page_response.text()
+            soup = bs(page_html, "lxml")
+            all_books_on_page = soup.find_all("div", class_="catalog__item")
+            all_items = [book.find("a")["href"] for book in all_books_on_page]
+            for item in all_items:
+                task = asyncio.create_task(get_item_data(session, item))
+                tasks.append(task)
+        except Exception as e:
+            page_error.append(page_link)
+            logger.exception(f"Exception on page {page_link} - {e}")
+            with open("page_error.txt", "a+") as file:
+                file.write(f"{page_link} + - + {e}")
+
+
 async def get_gather_data():
-    semaphore = asyncio.Semaphore(10)
+    # semaphore = asyncio.Semaphore(10)
     logger.info("Start to collect data")
     async with aiohttp.ClientSession(
         connector=aiohttp.TCPConnector(ssl=False), trust_env=True
@@ -152,34 +214,46 @@ async def get_gather_data():
         tasks = []
         logger.info(f"Page find - {max_pagination}")
         for page in range(1, int(max_pagination) + 1):
-            async with semaphore:
-                try:
-                    page_response = await session.get(
-                        f"{BASE_URL}/books/?page={page}", headers=headers
-                    )
-                    page_html = await page_response.text()
-                    soup = bs(page_html, "lxml")
-                    all_books_on_page = soup.find_all("div", class_="catalog__item")
-                    all_items = [book.find("a")["href"] for book in all_books_on_page]
-                    for item in all_items:
-                        task = asyncio.create_task(get_item_data(session, item))
-                        tasks.append(task)
-                except Exception as e:
-                    logger.exception(f"Exception on page {page} - {e}")
-                    with open("page_error.txt", "a+") as file:
-                        file.write(f"{page} + - + {e}")
-
+            page_link = f"{BASE_URL}/books/?page={page}"
+            await get_page_data(session, page_link, tasks)
         await asyncio.gather(*tasks)
+
+        # Reparse page errors
+        page_error_tasks = []
+        if page_error:
+            logger.warning("Reparse page errors")
+            for i in page_error:
+                await get_page_data(session, i, page_error_tasks)
+            await asyncio.gather(*page_error_tasks)
+
+        # Reparse item errors
+        items_error_tasks = []
+        if item_error:
+            logger.warning("Reparse item errors")
+            for i in item_error:
+                task = asyncio.create_task(get_item_data(session, i))
+                items_error_tasks.append(task)
+            await asyncio.gather(*items_error_tasks)
+
         print()
         logger.success("Finish collect data")
 
 
 @logger.catch
 def main():
-    logger.add(f"error.log", format="{time} {level} {message}", level="ERROR")
     asyncio.run(get_gather_data())
     logger.info("Start to write data in file")
-    pd.DataFrame(result).to_excel("msk_result.xlsx", index=False)
+    write_result_files(
+        base_dir=BASE_LINUX_DIR,
+        prefix="msk",
+        all_books_result=result,
+        id_to_add=id_to_add,
+        id_to_del=id_to_del,
+        not_in_sale=not_in_sale,
+        df_price_one=df_price_one,
+        df_price_two=df_price_two,
+        df_price_three=df_price_three,
+    )
     logger.success("Data was wrote in file successfully")
 
 
