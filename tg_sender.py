601531a95bebb32221011428d13b1a56e3abc461
roman <s-3m@yandex.ru>
2025-03-07 11:18:11 +0300

    delete unnecessary loger

--- tg_sender.py
+++ tg_sender.py
@@ -8,15 +8,12 @@ from loguru import logger
 
 @logger.catch
 async def tg_send_files(files: list[str], subject):
-    logger.info(f"Init Tg Bot for sending {subject}")
     bot = Bot(os.getenv("BOT_TOKEN"))
     files_list = []
-    logger.info(f"Attach files ({subject})")
     for file in files:
         file = FSInputFile(file)
         file = InputMediaDocument(media=file, caption=subject)
         files_list.append(file)
-    logger.info(f"Start sending files ({subject})")
     await bot.send_media_group(os.getenv("CHAT_ID"), files_list)
     await bot.session.close()
     logger.success(f"Files ({subject}) was send successfully")
