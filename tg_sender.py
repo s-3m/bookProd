d81557b3b6f5e60e3d861170b92ed73b38327de2
roman <s-3m@yandex.ru>
2025-03-07 11:04:33 +0300

    add tg send msg

--- tg_sender.py
+++ tg_sender.py
@@ -5,27 +5,45 @@ import os
 from aiogram.types.input_media_document import InputMediaDocument
 from loguru import logger
 
+
 @logger.catch
-async def tg_send_files(files:list[str], subject):
-    logger.info(f'Init Tg Bot for sending {subject}')
-    bot = Bot(os.getenv('BOT_TOKEN'))
+async def tg_send_files(files: list[str], subject):
+    logger.info(f"Init Tg Bot for sending {subject}")
+    bot = Bot(os.getenv("BOT_TOKEN"))
     files_list = []
-    logger.info(f'Attach files ({subject})')
+    logger.info(f"Attach files ({subject})")
     for file in files:
         file = FSInputFile(file)
         file = InputMediaDocument(media=file, caption=subject)
         files_list.append(file)
-    logger.info(f'Start sending files ({subject})')
-    await bot.send_media_group(os.getenv('CHAT_ID'), files_list)
+    logger.info(f"Start sending files ({subject})")
+    await bot.send_media_group(os.getenv("CHAT_ID"), files_list)
     await bot.session.close()
-    logger.success(f'Files ({subject}) was send successfully')
+    logger.success(f"Files ({subject}) was send successfully")
 
 
+@logger.catch
+async def tg_send_msg(prefix):
+    bot = Bot(os.getenv("BOT_TOKEN"))
+    await bot.send_message(
+        os.getenv("CHAT_ID"),
+        f"После парсинга {prefix} обнаружено слишком много позиций с остатком 0."
+        f" Данные автоматически не загружены на OZON. Необходимо проверить данные вручную!",
+    )
 
-if __name__ == '__main__':
+    await bot.send_message(
+        os.getenv("CHAT_ID_TEST"),
+        f"После парсинга {prefix} обнаружено слишком много позиций с остатком 0."
+        f" Данные автоматически не загружены на OZON. Необходимо проверить данные вручную!",
+    )
+
+    await bot.session.close()
+
+
+if __name__ == "__main__":
     a = []
-    for dirpath, _, filenames in os.walk('mg/compare'):
+    for dirpath, _, filenames in os.walk("mg/compare"):
         for f in filenames:
             a.append(os.path.abspath(os.path.join(dirpath, f)))
 
-    asyncio.run(tg_send_files(a, 'test'))
+    asyncio.run(tg_send_files(a, "test"))
