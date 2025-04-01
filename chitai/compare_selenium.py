87d30bc458e65e4ab20632220eee022546785087
roman <s-3m@yandex.ru>
2025-04-01 17:32:40 +0300

    new headers and cookies

--- chitai/compare_selenium.py
+++ chitai/compare_selenium.py
@@ -9,6 +9,7 @@ import pandas.io.formats.excel
 from bs4 import BeautifulSoup as bs
 import asyncio
 import pandas as pd
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tg_sender import tg_send_files, tg_send_msg
@@ -22,18 +23,30 @@ pandas.io.formats.excel.ExcelFormatter.header_style = None
 headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "accept-language": "ru,en;q=0.9",
-    "cache-control": "no-cache",
-    # 'cookie': '__ddg1_=pvduRZr2PWRQfgBSlf6h; refresh-token=; tmr_lvid=03935c7450b807684f9dcd65334067b0; tmr_lvidTS=1719430836103; _ym_uid=1719430836919836282; _ym_d=1719430836; gdeslon.ru.__arc_domain=gdeslon.ru; gdeslon.ru.user_id=15254033-97f3-4a90-ae4b-740f08cf987d; _bge_ci=BA1.1.1122769529.1719430836; popmechanic_sbjs_migrations=popmechanic_1418474375998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1; flocktory-uuid=0241fd6a-c187-4d15-b067-6914fd7ea4c9-9; adrcid=A6I49NSU-aGn_FnD2kzLfwA; adrcid=A6I49NSU-aGn_FnD2kzLfwA; stDeIdU=c130f130-3401-4ce5-9052-788e665fbea5; _ymab_param=NOyyliya_BgJ0VOb3JL1PA1-1gyIOZRewTIGnSgxe-t2ci28PM-AMDZfHAuZzH4TsvmxnZPeYOHvYxXI9RgNC-VeR8Q; chg_visitor_id=470bb97a-014e-404d-9c3d-a67245b92f38; adid=173169335285853; analytic_id=1731693382619506; tagtag_aid=ca8994e09ac019fbd41e0fc168321848; tagtag_aid=ca8994e09ac019fbd41e0fc168321848; tagtag_aid=ca8994e09ac019fbd41e0fc168321848; origem=cityads; deduplication_cookie=cityads; deduplication_cookie=cityads; _ga_YVB4ZXMWPL=GS1.2.1731764757.1.1.1731765315.60.0.0; access-token=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwODY5NzcsImlhdCI6MTczMTkxODk3NywiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjA0ODlmNzEzNzQ4NjRiNjYxMGNiOGJmZTlmNTY1M2UxOWRkYzQ3NTAwMmZlNTc1MzNlYWViMzk0MGJhOGZlZTkiLCJ0eXBlIjoxMH0.Tq3QOvFYkYoTpnAT4MDH51OIaSENrWDhAYCpNpXbFPg; _ga=GA1.1.1903252348.1719430836; _ym_isad=2; acs_3=%7B%22hash%22%3A%22768a608b20ce960ff29026da95a81203ec583ad1%22%2C%22nextSyncTime%22%3A1732005380055%2C%22syncLog%22%3A%7B%22224%22%3A1731918980055%2C%221228%22%3A1731918980055%2C%221230%22%3A1731918980055%7D%7D; acs_3=%7B%22hash%22%3A%22768a608b20ce960ff29026da95a81203ec583ad1%22%2C%22nextSyncTime%22%3A1732005380055%2C%22syncLog%22%3A%7B%22224%22%3A1731918980055%2C%221228%22%3A1731918980055%2C%221230%22%3A1731918980055%7D%7D; adrdel=1731918980080; adrdel=1731918980080; domain_sid=L3BsIrNkQonH7entRBvC0%3A1731918980423; clickCityAdsID=7MRZ235eurZf323; epn_click_id=7MRZ235eurZf323; tmr_detect=0%7C1731918987403; __ddg9_=85.198.105.3; partner_name=cityads; mindboxDeviceUUID=0660d298-673e-43fc-8993-474c6e6cd4c8; directCrm-session=%7B%22deviceGuid%22%3A%220660d298-673e-43fc-8993-474c6e6cd4c8%22%7D; __ddg10_=1731930396; __ddg8_=Kj0vHcaaytcn7CsU; _ga_W0V3RXZCPY=GS1.1.1731930384.6.1.1731930398.0.0.0; _ga_6JJPBGS8QY=GS1.1.1731930384.6.1.1731930398.0.0.0; _ga_LN4Z31QGF4=GS1.1.1731930376.7.1.1731930404.32.0.1425492556',
+    "cache-control": "max-age=0",
+    # 'cookie': '__ddg9_=185.112.249.83; __ddg1_=R992fleg5s9uUqtWE49T; _pk_id.1.f5fe=b58316b62597714f.1743516792.; _pk_ses.1.f5fe=1; __ddgid_=QHi2XPU2bjRmnYcr; __ddgmark_=orfy5PTHwhmAnmj3; __ddg5_=rFtFVC5ujLXNhx3M; __ddg2_=EjiE0k6vT4io83Sc; __ddg8_=x2wIvvgyqOS3QoFQ; __ddg10_=1743516830',
     "priority": "u=0, i",
-    "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "YaBrowser";v="24.10", "Yowser";v="2.5"',
+    "referer": "https://www.chitai-gorod.ru/",
+    "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "YaBrowser";v="25.2", "Yowser";v="2.5"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
     "sec-fetch-dest": "document",
     "sec-fetch-mode": "navigate",
     "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 YaBrowser/25.2.0.0 Safari/537.36",
+}
+cookies = {
+    "__ddg9_": "185.112.249.83",
+    "__ddg1_": "R992fleg5s9uUqtWE49T",
+    "_pk_id.1.f5fe": "b58316b62597714f.1743516792.",
+    "_pk_ses.1.f5fe": "1",
+    "__ddgid_": "QHi2XPU2bjRmnYcr",
+    "__ddgmark_": "orfy5PTHwhmAnmj3",
+    "__ddg5_": "rFtFVC5ujLXNhx3M",
+    "__ddg2_": "EjiE0k6vT4io83Sc",
+    "__ddg8_": "x2wIvvgyqOS3QoFQ",
+    "__ddg10_": "1743516830",
 }
 
 DEBUG = True if sys.platform.startswith("win") else False
@@ -66,6 +79,7 @@ def get_link_from_ajax(article):
                 "https://web-gate.chitai-gorod.ru/api/v2/search/product",
                 headers=headers,
                 params=params,
+                cookies=cookies,
                 timeout=15,
             )
             response = resp.json()
@@ -91,7 +105,9 @@ def get_main_data(book_item):
 
         response_text = ""
         for _ in range(5):
-            response = requests.get(book_item["link"], headers=headers, timeout=30)
+            response = requests.get(
+                book_item["link"], headers=headers, timeout=30, cookies=cookies
+            )
             if response.status_code == 404:
                 book_item["stock"] = "0"
                 book_item["price"] = None
@@ -131,7 +147,8 @@ def get_main_data(book_item):
 
 
 async def get_auth_token():
-    resp = requests.get("https://www.chitai-gorod.ru", headers=headers)
+    resp = requests.get("https://www.chitai-gorod.ru", headers=headers, cookies=cookies)
+    time.sleep(15)
     ddd = resp.cookies
     acc_token = (
         str(ddd["access-token"])
@@ -222,4 +239,5 @@ def super_main():
 
 
 if __name__ == "__main__":
+    # main()
     super_main()
