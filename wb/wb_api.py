11cd976d7617a736631dae6666476d1662753781
Roman <s-3m@yandex.ru>
2026-05-02 01:17:47 +0300

    [add] - fork fine after test

--- wb/wb_api.py
+++ wb/wb_api.py
@@ -6,14 +6,20 @@ from loguru import logger
 
 class Wildberries:
     def __init__(self, api_token):
-        self.headers = {"Authorization": f"{api_token}"}
+        self.headers = {
+            "Authorization": f"{api_token}",
+            "Content-Type": "application/json",
+        }
 
     def get_items_list(self) -> list[dict[str, Any]]:
         result = []
         body = {
             "settings": {
                 "sort": {"ascending": True},
-                "filter": {},
+                "filter": {
+                    "withPhoto": -1,
+                    "allowedCategoriesOnly": False,
+                },
                 "cursor": {"limit": 100},
             }
         }
@@ -27,17 +33,21 @@ class Wildberries:
             raw_list = response.json()
             for i in raw_list["cards"]:
                 result.append(i)
-                if raw_list["cursor"]["total"] < 100:
-                    break
-                body["settings"]["cursor"] = raw_list["cursor"]
+            if raw_list["cursor"]["total"] < 100:
+                break
+            if len(result) >= 100:
+                break
+            body["settings"]["cursor"]["updatedAt"] = raw_list["cursor"]["updatedAt"]
+            body["settings"]["cursor"]["nmID"] = raw_list["cursor"]["nmID"]
 
         return result
 
-    def update_stocks(self, array_of_items: list[dict[str, str]]):
+    def update_stocks(self, array_of_items: list[dict[str, str]] = None):
         body_data = [
-            {"chrtId": i["article"], "amount": i["stock"]} for i in array_of_items
+            {"chrtId": i["chrtID"], "amount": int(i["stock"])} for i in array_of_items
         ]
-        warehouse_id = ""
+
+        warehouse_id = 1658946
 
         for i in range(0, len(body_data), 1000):
             body = {"stocks": body_data[i : i + 1000]}
@@ -51,3 +61,20 @@ class Wildberries:
                     logger.warning(f"Ошибка обновления остатков WB - {response.json()}")
             except Exception as e:
                 logger.exception(e)
+
+    def get_warehouses(self):
+        response = requests.get(
+            "https://marketplace-api.wildberries.ru/api/v3/warehouses",
+            headers=self.headers,
+        )
+        result = response.json()
+        return result
+
+    def get_items_stocks(self):
+        response = requests.post(
+            "https://marketplace-api.wildberries.ru/api/v3/stocks/1658946",
+            headers=self.headers,
+            json={"chrtIds": []},
+        )
+        result = response.json()
+        return result
